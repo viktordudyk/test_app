ba87d198e0aa76dadb8641852722e5411a26ae15
Viktor Dudyk <victordudyk@sigma.software>
2020-12-28 10:09:49 +0200

    'initial'

--- App.js
+++ App.js
@@ -65,7 +65,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 10,
+    borderRadius: 11,
     padding: 30
   }, 
   text: {
